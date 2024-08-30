0c212c286fdc58f6f695bc49cc42bdf21ede7260
ManeaRoliste <gwendal.maxime@gmail.com>
2024-08-30 03:31:05 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,24 +2,24 @@ const {google} = require("googleapis");
 const {GoogleAuth} = require("google-auth-library");
 const {fs} = require("fs/promises");
 
-const credentials = await fs.readFile('credentials.json', 'utf-8');
+async function editHexmap(hexmap){
+  const credentials = await fs.readFile('credentials.json', 'utf-8');
 
-let credentials_parsed = JSON.parse(credentials);
+  let credentials_parsed = JSON.parse(credentials);
 
-const auth = new GoogleAuth({
-  credentials_parsed,
-  scopes: ['https://www.googleapis.com/auth/spreadsheets'],
-});
+  const auth = new GoogleAuth({
+    credentials_parsed,
+    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+  });
 
-const authClient = auth.fromJSON(credentials_parsed);
+  const authClient = auth.fromJSON(credentials_parsed);
 
-const sheets = google.sheets({ version: 'v4', auth: authClient });
+  const sheets = google.sheets({ version: 'v4', auth: authClient });
 
-function editHexmap(hexmap){
   sheets.spreadsheets.values.update({
     spreadsheetId:"1Spq-Xf83L65rQlLtYvNQO22BevyMSqjeRr-r6IRvn_o",
     range:"Feuille 1!A1",
     valueInputOption: "RAW",
-    resource:{values:[["Hello world"]]}
+    resource:{values:[[JSON.stringify(hexmap)]]}
   });
 }
