9275359a1a95e1b9692b0ffdd3a8c70c26557df3
ManeaRoliste <gwendal.maxime@gmail.com>
2024-08-30 02:53:36 +0200

    Set

---
+++ server.js
@@ -0,0 +1,23 @@
+import { google } from "googleapis";
+import { GoogleAuth } from "google-auth-library";
+import fs from "fs/promises";
+
+const credentials = await fs.readFile('credentials.json', 'utf-8');
+
+let credentials_parsed = JSON.parse(credentials);
+
+const auth = new GoogleAuth({
+  credentials_parsed,
+  scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+});
+
+const authClient = auth.fromJSON(credentials_parsed);
+
+const sheets = google.sheets({ version: 'v4', auth: authClient });
+
+sheets.spreadsheets.values.update({
+  spreadsheetId:"1Spq-Xf83L65rQlLtYvNQO22BevyMSqjeRr-r6IRvn_o",
+  range:"Feuille 1!A1",
+  valueInputOption: "RAW",
+  resource:{values:[["Hello world"]]}
+});
